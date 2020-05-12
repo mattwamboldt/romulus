8cabe96cd55122f298ee54aa53d31fd9b8a1d068
Matt Wamboldt <mattwamboldt@hotmail.com>
2020-05-11 23:02:30 -0300

    Moving typedefs into a common header

--- connessa/src/main.cpp
+++ connessa/src/main.cpp
@@ -2,16 +2,7 @@
 #include <conio.h>
 #include <windows.h>
 
-// Some common typedefs that I tend to use for uniformity
-typedef char int8;
-typedef short int16;
-typedef int int32;
-typedef long long int64;
-
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long long uint64;
+#include "common.h"
 
 struct CPU
 {
