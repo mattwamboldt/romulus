c07351790c97370403082051bc06c8b1b40eab78
Matt Wamboldt <mattwamboldt@hotmail.com>
2022-12-07 17:44:37 -0400

    [NES][PPU] Status register and bit defines
    Added a pile of TODOs on things that are just being named but not used

--- source/nes/ppu.cpp
+++ source/nes/ppu.cpp
@@ -15,12 +15,12 @@ bool PPU::isNMIFlagSet()
 
 void PPU::setControl(uint8 value)
 {
-    nmiEnabled = value & 0x80;
+    nmiEnabled = value & BIT_7;
     // NOTE: Bit 6 is tied to ground on the NES. So its safe to ignore
-    useTallSprites = value & 0x20;
-    backgroundPatternBaseAddress = (value & 0x10) ? 0x1000 : 0; // PERF: Could do shifts here to not branch
-    spritePatternBaseAddress = (value & 0x08) ? 0x1000 : 0;
-    vramAddressIncrement = (value & 0x04) ? 32 : 1;
+    useTallSprites = value & BIT_5;
+    backgroundPatternBaseAddress = (value & BIT_4) ? 0x1000 : 0; // PERF: Could do shifts here to not branch
+    spritePatternBaseAddress = (value & BIT_3) ? 0x1000 : 0;
+    vramAddressIncrement = (value & BIT_2) ? 32 : 1;
     baseNametableAddress = 0x2000 + (0x0400 * (value & 0x03));
 }
 
@@ -31,8 +31,24 @@ void PPU::setMask(uint8 value)
 
 uint8 PPU::getStatus()
 {
-    OutputDebugStringA("[PPU] getStatus\n");
-    return 0;
+    uint8 status = 0;
+    if (isSpriteOverflowFlagSet)
+    {
+        status |= BIT_5;
+    }
+
+    if (isSpriteZeroHit)
+    {
+        status |= BIT_6;
+    }
+
+    if (nmiRequested)
+    {
+        status |= BIT_7;
+    }
+
+    nmiRequested = false;
+    return status;
 }
 
 void PPU::setOamAddress(uint8 value)
--- source/nes/ppu.h
+++ source/nes/ppu.h
@@ -6,6 +6,7 @@ class PPU
 {
 public:
     void connect(IBus* bus) { this->bus = bus; }
+    // TODO: Implement
     void tick();
 
     bool isNMIFlagSet();
@@ -13,18 +14,26 @@ public:
     // CPU <=> PPU Bus functions
 
     void setControl(uint8 value);
+    // TODO: Implement
     void setMask(uint8 value);
     
     uint8 getStatus();
 
+    // TODO: Implement
     void setOamAddress(uint8 value);
+    // TODO: Implement
     void setOamData(uint8 value);
+    // TODO: Implement
     uint8 getOamData();
 
+    // TODO: Implement
     void setScroll(uint8 value);
+    // TODO: Implement
     void setAddress(uint8 value);
 
+    // TODO: Implement
     void setData(uint8 value);
+    // TODO: Implement
     uint8 getData();
 
     // TODO: This probably doens't make sense to happen in the PPU actually
@@ -37,12 +46,26 @@ public:
 private:
     IBus* bus;
 
+    // TODO: Implement
+    // Set at the start of vblank: dot 1 of scanline 241
+    // Cleared at the end of vblank: dot 1 of pre-render
     bool nmiRequested;
     bool nmiEnabled;
 
+    // TODO: Implement
     bool useTallSprites;
+    // TODO: Implement
     uint16 baseNametableAddress;
+    // TODO: Implement
     uint16 vramAddressIncrement;
+    // TODO: Implement
     uint16 spritePatternBaseAddress;
+    // TODO: Implement
     uint16 backgroundPatternBaseAddress;
+
+    // TODO: Implement
+    // Apparently complicated do to bugs? https://www.nesdev.org/wiki/PPU_sprite_evaluation
+    bool isSpriteOverflowFlagSet;
+    // TODO: Implement
+    bool isSpriteZeroHit;
 };
